5bae43ebe6cd48a678790ef0b0189f35e90d6215
Andrei Bratu <bratu.andrei@outlook.co>
2019-06-08 19:12:23 +0300

    Firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB-Ik8lA940Ywb6LmDaGe-vkW9OSvMlOmw",
+    authDomain: "bpay-225c1.firebaseapp.com",
+    databaseURL: "https://bpay-225c1.firebaseio.com",
+    projectId: "bpay-225c1",
+    storageBucket: "bpay-225c1.appspot.com",
+    messagingSenderId: "912111041138",
+    appId: "1:912111041138:web:8c9154c880a5c978"
+  }
 };
 
 /*
